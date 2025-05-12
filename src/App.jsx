5fab8b50aeaf4191782240c18259c1d74791186b
joesanthosh24 <joe.santhosh88@yahoo.com>
2025-05-12 11:12:45 -0400

    Fixed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/home/home.page';
 import { NotFoundPage } from './pages/not-found/not-found.page';
 
